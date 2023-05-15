1025acff9fcb7ec01c23f154c47526c59053cccd
isaque14 <isaquediniz14@gmail.com>
2023-05-14 22:46:08 -0300

    feat(player): movimento Horizontal

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    public const float Speed = 4;
+    private Rigidbody2D _rbPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rbPlayer = GetComponent<Rigidbody2D>();        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float horizontalMoviment = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(horizontalMoviment * Time.deltaTime * Speed, 0, 0);
+        _rbPlayer.velocity = new Vector2(horizontalMoviment * Speed, _rbPlayer.velocity.y);
     }
 }
